5d3d1a7bb00e5a92fbed0b863e5288596bedb00a
YuqinHu <h878688736@gmail.com>
2023-04-07 19:45:59 -0700

    finish day list item

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,12 @@
 import React from "react";
 import classNames from "classnames";
-
 import "components/Button.scss";
 
 export default function Button(props) {
    const buttonClass = classNames("button", {
       "button--confirm": props.confirm,
       "button--danger": props.danger
-   });
+    });
 
    return (
       <button
